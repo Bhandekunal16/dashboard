589b3f65b97ac23159a61d07672294021838a1a3
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-21 17:56:30 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,20 +12,20 @@ const Home = () => {
           featured content, products, or services here.
         </p>
         <div style={{padding:"2%"}}>
-        <Link to="https://github.com/Bhandekunal16/Logic/tree/main/c" style={{textDecoration:"none", color:"orange"}}>Introduction to C Programming Basics</Link>
+        <Link to="https://github.com/Bhandekunal16/Logic/tree/main/c" style={{textDecoration:"none", color:"black"}}>Introduction to C Programming Basics</Link>
         </div>
         <div style={{padding:"2%"}}>
-        <Link to="https://github.com/Bhandekunal16/Logic/tree/main/python" style={{textDecoration:"none", color:"orange"}}>Introduction to python Programming Basics</Link>
+        <Link to="https://github.com/Bhandekunal16/Logic/tree/main/python" style={{textDecoration:"none", color:"black"}}>Introduction to python Programming Basics</Link>
         </div>
         <div style={{padding:"2%"}}>
-        <Link to="https://github.com/Bhandekunal16/Logic/tree/main/javascript" style={{textDecoration:"none", color:"orange"}}>Introduction to javascript Programming Basics</Link>
+        <Link to="https://github.com/Bhandekunal16/Logic/tree/main/javascript" style={{textDecoration:"none", color:"black"}}>Introduction to javascript Programming Basics</Link>
         </div>
       </main>
       </div>
       <div className="ui">
         <div className="ui-home">
           <h2>game</h2>
-          <Link style={{color: "orange", padding:"2%"}} to="https://bhandekunal16.github.io/game/">Robotic</Link>
+          <Link style={{color: "black", padding:"2%", textDecoration:"none"}} to="https://bhandekunal16.github.io/game/">Robotic</Link>
         </div>
       </div>
     </div>
