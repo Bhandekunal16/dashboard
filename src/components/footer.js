e4101e6b27ab62730571fae039c58568c998f7a0
smart school <you@example.com>
2023-08-22 21:06:28 +0530

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footerContent">
-      <h3 style={{color: "skyblue", padding: "5px 5px"}}>Robotic<span style={{color:"blueviolet"}}>'s</span></h3>
+      <Link  to ="https://theme-pr6a747xx-bhandekunal16.vercel.app/" style={{color: "skyblue", padding: "5px 5px"}}>Robotic<span style={{color:"blueviolet"}}>'s</span></Link>
     </div>
   );
 };
