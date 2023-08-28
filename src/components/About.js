8c64472abf9fd5c6136755f9d5fa20be73c27b4f
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-28 13:18:47 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ const About = () => {
   const [project, setProject] = useState([]);
   function time() {
     const timing = new Date().getHours();
-    let goodmorning = "good morning";
-    let goodAfternoon = "good afternoon";
-    let goodEvening = "good evening";
-    let goodNight = "good night";
+    let goodmorning = "Good morning";
+    let goodAfternoon = "Good afternoon";
+    let goodEvening = "Good evening";
+    let goodNight = "Good night";
 
     switch (true) {
       case timing < 12:
@@ -57,31 +57,42 @@ const About = () => {
   }, []);
 
   return (
+    <>
     <div style={{ display: "flex", justifyContent: "center", padding: "2%" }}>
       <div
         style={{
           textAlign: "center",
           backgroundColor: "whitesmoke",
           width: "90vw",
-          height: "80vh",
+          height: "40vh",
           borderRadius: "10px",
         }}
       >
-        <h2 style={{ color: "skyblue" }}>Our Company</h2>
+        <h2 style={{ color: "skyblue" }}>About us</h2>
         <p style={{ color: "orange" }}>{new Date().toISOString()}</p>
         <h1 style={{ color: "orange" }}>{time()}</h1>
         <div>
-          <Link to="/youtubeList" style={{ color: "gray" }}>
+          <Link to="/youtubeList" style={{ color: "gray", textDecoration:"none" }}>
             Youtube vedio's : {JSON.stringify(data.data)}
           </Link>
         </div>
         <div>
-          <Link to="/projectList" style={{ color: "gray" }}>
-            project's count : {JSON.stringify(project.data)}
+          <Link to="/projectList" style={{ color: "gray",textDecoration:"none" }}>
+            Project's count : {JSON.stringify(project.data)}
           </Link>
         </div>
       </div>
+      
     </div>
+  <div style={{backgroundColor: "whitesmoke",
+          width: "100vw",
+          height: "40vh",
+          borderRadius: "10px",}}>
+    <h1 style={{textAlign:"center", color:"skyblue"}}>team</h1>
+    <p style={{marginLeft:"10%", color:"gray"}}>name : kunal eknath bhande</p>
+    <p style={{marginLeft:"10%", color:"gray"}}> work : backend/frontend lead & project management.</p>
+  </div>
+  </>
   );
 };
 
