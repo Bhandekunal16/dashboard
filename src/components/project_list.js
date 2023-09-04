afac388f3ba88335dff157482bf826ba70fa1764
bhandekuanl16 <bhandekunal16@gmail.com>
2023-09-04 17:53:17 +0530

    update

--- src/components/project_list.js
+++ src/components/project_list.js
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { fetchProjectData } from "../service/app-service";
 import "./ProjectList.css";
+import axios from 'axios';
 
 const ProjectList = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+
   const fetchDataFromAPI = async () => {
     try {
-      const apiData = await fetchProjectData(
-        "https://robotec-dashboard-k3moev00e-bhandekunal16.vercel.app/project/getallproject"
+   
+      const response = await axios.get(
+        'https://robotec-dashboard-k3moev00e-bhandekunal16.vercel.app/project/getallproject'
       );
-      setData(apiData.data);
+  
+      console.log(response);
+      const apiData = response.data.data;
+  
+      // Check if data is empty or equals 0
+      if (apiData === null ) {
+        // Handle the case where data is empty
+        Setmessage('No data available');
+      } else {
+        // Set the data when it's not empty
+        setData(apiData);
+      }
+  
     } catch (error) {
-      setError("Error fetching data");
+      
+      setError('Error fetching data');
     } finally {
+     
       setLoading(false);
     }
   };
--- src/components/youtube_list.js
+++ src/components/youtube_list.js
@@ -1,24 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { fetchYoutubeData } from "../service/app-service";
 import "./youtubeList.css";
+import axios from 'axios';
 
 const YoutubeList = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [setmessage, Setmessage] = useState(null)
 
-  const fetchDataFromAPI = async () => {
-    try {
-      const apiData = await fetchYoutubeData(
-        "https://robotec-dashboard-k3moev00e-bhandekunal16.vercel.app/youtube/getCount1"
-      );
-      setData(apiData.data);
-    } catch (error) {
-      setError("Error fetching data");
-    } finally {
-      setLoading(false);
+ 
+
+const fetchDataFromAPI = async () => {
+  try {
+ 
+    const response = await axios.get(
+      'https://robotec-dashboard-k3moev00e-bhandekunal16.vercel.app/youtube/getAllYoutube'
+    );
+
+    console.log(response);
+    const apiData = response.data.data;
+
+    // Check if data is empty or equals 0
+    if (apiData === null ) {
+      // Handle the case where data is empty
+      Setmessage('No data available');
+    } else {
+      // Set the data when it's not empty
+      setData(apiData);
     }
-  };
+
+  } catch (error) {
+    
+    setError('Error fetching data');
+  } finally {
+   
+    setLoading(false);
+  }
+};
+
 
   useEffect(() => {
     fetchDataFromAPI();
@@ -28,6 +48,7 @@ const YoutubeList = () => {
     <div className="youtube-list-container">
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
+      {setmessage && <p>{setmessage}</p>}
       {data.length > 0 && (
         <table className="youtube-table">
           <thead>
