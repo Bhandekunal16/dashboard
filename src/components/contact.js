bd228a908ab1116a622a5b5f329c64dafcb40fc5
bhandekuanl16 <bhandekunal16@gmail.com>
2023-09-05 11:23:04 +0530

    update ui

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const Contact = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const fetchDataFromAPI = async () => {
+    try {
+      const response = await axios.post("https://robotec-dashboard-7u265udaf-bhandekunal16.vercel.app/get/ButtonUI", {
+        data: `Send Email`,
+      });
+      console.log(response);
+      const button = document.getElementById("submit");
+      button.innerHTML = response.data;
+    } catch (error) {
+      setError("Error fetching data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromAPI();
+  }, []);
   return (
     <div className="primary">
       <form className="contact-form">
@@ -13,7 +35,7 @@ const Contact = () => {
         <label>Message:</label>
         <textarea name="text" rows="4" required></textarea>
         <br />
-        <button type="submit">Send Email</button>
+        <div id="submit"></div>
       </form>
     </div>
   );
