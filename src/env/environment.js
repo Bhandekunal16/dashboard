5ad64d9bf193bbd9374abac89b717ee05adb86f7
bhandekuanl16 <bhandekunal16@gmail.com>
2023-09-05 14:13:46 +0530

    update

--- src/env/environment.js
+++ src/env/environment.js
@@ -1,5 +1,5 @@
 export const environment= {
-    base_url:'robotec-dashboard-2tnnokoxf-bhandekunal16.vercel.app',
+    base_url:'robotec-dashboard-h6v4zfhf9-bhandekunal16.vercel.app',
     inst:'www.instagram.com/roboticdevlopments01/?igshid=YTQwZjQ0NmI0OA%3D%3D',
     Youtube:'www.youtube.com/@R0b0tics-Devel0pment'
 }
