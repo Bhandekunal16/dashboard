27507e9badc2cefed9ffe0064899a35c45844d45
bhandekuanl16 <bhandekunal16@gmail.com>
2023-09-08 14:12:24 +0530

    update

--- src/env/environment.js
+++ src/env/environment.js
@@ -1,11 +1,12 @@
 export const environment = {
-  base_url: "robotec-dashboard-h6v4zfhf9-bhandekunal16.vercel.app",
+  base_url: "robotec-dashboard-mr7zjtcxz-bhandekunal16.vercel.app",
   inst: "www.instagram.com/roboticdevlopments01/?igshid=YTQwZjQ0NmI0OA%3D%3D",
   Youtube: "www.youtube.com/@R0b0tics-Devel0pment",
   c: "github.com/Bhandekunal16/Logic/tree/main/c",
   python: "github.com/Bhandekunal16/Logic/tree/main/python",
   javascript: "github.com/Bhandekunal16/Logic/tree/main/javascript",
 };
+
 
 export const programiz = {
   c_compiler: "www.programiz.com/c-programming/online-compiler",
