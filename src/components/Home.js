aa42d1bafdee0e68688a6472e1c3804b4a6b6252
bhandekunal16 <bhandekunal16@gmail.com>
2023-09-21 10:54:15 +0530

    learn props

--- src/components/Home.js
+++ src/components/Home.js
@@ -166,7 +166,7 @@ const Home = () => {
         style={{
           width: "100vw",
           height: "auto",
-          padding : "1%",
+          padding : "5%",
         }}
       >
         <ChartUI data={data} project={project} insta={insta} />
