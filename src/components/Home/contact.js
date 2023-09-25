931e45caea1330004068b6289736c6137dbb6a30
bhandekunal16 <bhandekunal16@gmail.com>
2023-09-25 10:32:45 +0530

    update

--- src/components/Home/contact.js
+++ src/components/Home/contact.js
@@ -2,86 +2,35 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { environment, method } from "../../env/environment";
 
-        
-
-// const Contact = () => {
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const fetchDataFromAPI = async () => {
-//     try {
-//       const response = await axios.post(
-//         `${method.http}://${environment.base_url}/get/ButtonUI`,
-//         {
-//           data: `Send Email`,
-//         }
-//       );
-//       console.log(response);
-//       const button = document.getElementById("submit");
-//       button.innerHTML = response.data;
-//     } catch (error) {
-//       setError("Error fetching data");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   function Submit () {
-//     const to = document.getElementsByTagName("to");
-//     const message = document.getElementsByTagName("message");
-//     console.log(message.value);
-//   };
-
-//   useEffect(() => {
-//     fetchDataFromAPI();
-//   }, []);
-//   return (
-//     <div className="primary">
-//       <form className="contact-form">
-//         <label>To:</label>
-//         <input type="email" name="to" required />
-//         <br />
-//         <label>Message:</label>
-//         <textarea name="text" rows="4" required id="message"></textarea>
-//         <br />
-//         <div id="submit" onClick={Submit()}></div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
-
 const Contact = () => {
   const [emailData, setEmailData] = useState({
-    to: '',
-    message: '',
+    to: "",
+    message: "",
   });
 
   const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const fetchDataFromAPI = async () => {
-      try {
-        const response = await axios.post(
-          `${method.http}://${environment.base_url}/get/ButtonUI`,
-          {
-            data: `Send Email`,
-          }
-        );
-        console.log(response);
-        const button = document.getElementById("submit");
-        button.innerHTML = response.data;
-      } catch (error) {
-        setError("Error fetching data");
-      } finally {
-        setLoading(false);
-      }
-    };
+  const [error, setError] = useState(null);
+  const fetchDataFromAPI = async () => {
+    try {
+      const response = await axios.post(
+        `${method.http}://${environment.base_url}/get/ButtonUI`,
+        {
+          data: `Send Email`,
+        }
+      );
+      console.log(response);
+      const button = document.getElementById("submit");
+      button.innerHTML = response.data;
+    } catch (error) {
+      setError("Error fetching data");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    useEffect(() => {
-          fetchDataFromAPI();
-        }, []);
-  
+  useEffect(() => {
+    fetchDataFromAPI();
+  }, []);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -97,32 +46,35 @@ const Contact = () => {
     try {
       const response = await sendEmail(emailData);
       // Handle success, e.g., show a success message
-      console.log('Email sent successfully:', response);
+      console.log("Email sent successfully:", response);
 
       // Clear the form
       setEmailData({
-        to: '',
-        message: '',
+        to: "",
+        message: "",
       });
     } catch (error) {
       // Handle error, e.g., show an error message
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
     }
   };
 
   const sendEmail = async (data) => {
     try {
-      const response = await fetch(`${method.http}://${environment.base_url}/message/send-email`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+      const response = await fetch(
+        `${method.http}://${environment.base_url}/message/send-email`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
       console.log(JSON.stringify(data));
       if (!response.ok) {
-        throw new Error('Failed to send email');
+        throw new Error("Failed to send email");
       }
 
       return response.json();
