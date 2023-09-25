d86ca79cb456097b739c75c064220cd5e0518fb3
bhandekunal16 <bhandekunal16@gmail.com>
2023-09-25 12:40:49 +0530

    update

--- src/components/Home/service.js
+++ src/components/Home/service.js
@@ -12,111 +12,6 @@ const Service = () => {
 
   return (
     <>
-      <div className="primary" style={{ padding: "1%" }}>
-        <div
-          style={{
-            textAlign: "center",
-            backgroundColor: color().primary,
-            borderRadius: "10px",
-            boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.3)",
-          }}
-        >
-          <div className="service-item">
-            <h2 style={{ color: "skyBlue" }}>Web Development</h2>
-            <p style={{ color: color().text }}>
-              We offer professional web development services to build responsive
-              and user-friendly websites.
-            </p>
-            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-              <div>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.html_creator}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    online html creator
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.html_routing}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    javascript base routing templates
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.web_application}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    web page template
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.binary_converter}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    binary-converter
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.robotic_project}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    mathematical calculator
-                  </Link>
-                </p>
-              </div>
-
-              <div>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.time}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    time
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.robotec_dashboard}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    Live working web portal
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.cube_finder}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    cube founder
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.squre_finder}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    square founder
-                  </Link>
-                </p>
-                <p>
-                  <Link
-                    to={`${method.http}://${project.glowing}`}
-                    style={{ textDecoration: "none", color: color().text }}
-                  >
-                    glowing cube design
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div
         className="service-item"
         style={{
@@ -147,6 +42,300 @@ const Service = () => {
           Our digital marketing services include SEO, social media marketing,
           and content marketing.
         </p>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: "2%",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.html_creator}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : online html creator
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : html web page that where you write the html code and
+            get result in same place
+          </p>
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.html_routing}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : javascript base routing templates
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : html web template.
+          </p>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: "2%",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.web_application}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : web page template
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : html web page template
+          </p>
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.binary_converter}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : binary-converter
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : take the input and convert it on binary form .
+          </p>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: "2%",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.robotic_project}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : mathematical calculator
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : calculator portal.
+          </p>
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.time}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : time
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : time showing portal.
+          </p>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: "2%",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.robotec_dashboard}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : angular web application template
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : Live working web portal.
+          </p>
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.cube_finder}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : cube founder
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : find a cube of any given number.
+          </p>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: "2%",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.squre_finder}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : square founder
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>
+            description : give the square of any given number.
+          </p>
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            height: "auto",
+            width: "40vw",
+            backgroundColor: color().primary,
+            borderRadius: "10px",
+          }}
+        >
+          <h3
+            style={{
+              backgroundColor: color().sec,
+            }}
+          >
+            <Link
+              to={`${method.http}://${project.glowing}`}
+              style={{ textDecoration: "none", color: color().text }}
+            >
+              project name : glowing cube design
+            </Link>
+          </h3>
+          <p style={{ color: color().text }}>description : glowing template.</p>
+        </div>
       </div>
     </>
   );
