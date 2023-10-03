4c48c6005b8a908a2057bd0326b7ebf148699125
bhandekunal16 <bhandekunal16@gmail.com>
2023-10-03 17:32:23 +0530

    update

--- src/components/Home/assistant.js
+++ src/components/Home/assistant.js
@@ -16,65 +16,81 @@ const Assistant = () => {
     const ans = document.getElementById("output");
     const mathematical = document.getElementById("math");
 
-    if (input.value === "table") {
-      mathematical.style.display = "block";
-      const tableResult = math(input.value, mathematical.value);
-      console.log(tableResult);
-      ans.innerHTML =
-        tableResult.one +
-        "," +
-        tableResult.two +
-        "," +
-        tableResult.three +
-        "," +
-        tableResult.four +
-        "," +
-        tableResult.five +
-        "," +
-        tableResult.six +
-        "," +
-        tableResult.seven +
-        "," +
-        tableResult.eight +
-        "," +
-        tableResult.nine +
-        "," +
-        tableResult.ten;
-    } else if (input.value == "log") {
-      mathematical.style.display = "block";
-      const log = output(input.value, mathematical.value);
-    } else if (input.value == "random number") {
-      mathematical.style.display = "block";
-      const random = output(input.value, mathematical.value);
-     
-      console.log(test);
-      ans.innerHTML = random;
-    } else if (input.value == "convert binary") {
-      mathematical.style.display = "block";
-      const binary = convertToBinary(mathematical.value);
-      ans.innerHTML = binary;
-    } else if (input.value == "convert binary to string") {
-      mathematical.style.display = "block";
-      const binary = convertToString(mathematical.value);
-      ans.innerHTML = binary;
-    } else if (input.value == "convert to hex") {
-      mathematical.style.display = "block";
-      const hex = hexadecimalConvert(mathematical.value);
-      ans.innerHTML = hex;
-    } else if (input.value == "convert hex to string") {
-      mathematical.style.display = "block";
-      const hex = hexToString(mathematical.value);
-      ans.innerHTML = hex;
+    switch (input.value) {
+      case "table":
+        mathematical.style.display = "block";
+        const tableResult = math(input.value, mathematical.value);
+        console.log(tableResult);
+        ans.innerHTML =
+          tableResult.one +
+          "," +
+          tableResult.two +
+          "," +
+          tableResult.three +
+          "," +
+          tableResult.four +
+          "," +
+          tableResult.five +
+          "," +
+          tableResult.six +
+          "," +
+          tableResult.seven +
+          "," +
+          tableResult.eight +
+          "," +
+          tableResult.nine +
+          "," +
+          tableResult.ten;
+        break;
+    
+      case "log":
+        mathematical.style.display = "block";
+        const log = output(input.value, mathematical.value);
+        break;
+    
+      case "random number":
+        mathematical.style.display = "block";
+        const random = output(input.value, mathematical.value);
+        console.log(test);
+        ans.innerHTML = random;
+        break;
+    
+      case "convert binary":
+        mathematical.style.display = "block";
+        const binary = convertToBinary(mathematical.value);
+        ans.innerHTML = binary;
+        break;
+    
+      case "convert binary to string":
+        mathematical.style.display = "block";
+        const string = convertToString(mathematical.value);
+        ans.innerHTML = string;
+        break;
+    
+      case "convert to hex":
+        mathematical.style.display = "block";
+        const Hex = hexadecimalConvert(mathematical.value);
+        ans.innerHTML = Hex;
+        break;
+    
+      case "convert hex to string":
+        mathematical.style.display = "block";
+        const hex = hexToString(mathematical.value);
+        ans.innerHTML = hex;
+        break;
+    
+      case 'calculate':
+        mathematical.style.display = "block";
+        const calculate = calculation(mathematical.value);
+        ans.innerHTML = calculate;
+        break;
+    
+      default:
+        const methodResult = output(input.value);
+        ans.innerHTML = methodResult;
+        break;
     }
-    else if (input.value == 'calculate'){
-      mathematical.style.display = "block";
-      const calculate = calculation(mathematical.value);
-      ans.innerHTML = calculate
-    }
-    else {
-      const methodResult = output(input.value);
-      ans.innerHTML = methodResult;
-    }
+    
   }
 
   return (
